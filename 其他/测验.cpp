f7bd2b4b95752ed02f36848f6fa3a9677de90ee3
AriaFAll <ariafall186@gmail.com>
2026-06-21 01:13:28 +0800

    更新：修改测验.cpp，新增acm版线段树.cpp

--- 其他/测验.cpp
+++ 其他/测验.cpp
@@ -1,31 +1,66 @@
-#include<bits/stdc++.h>
-#define int long long
-#define endl "\n"
-#define vt vector
-#define pb push_back
+#include <iostream>
+#include <vector>
+#include <queue>
+#include<functional>
 using namespace std;
+#define int long long
+const int INF = 1e9+5;
+#define vt vector
+#define PII pair<int,int>
 
-signed main(){
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n;
-    cin>>n;
-    vt<int> x(n+5);
-    vt<int> y(n+5);
-    int cnt=0;
-    for(int i=0;i<n;i++){
-      cin>>x[i]>>y[i];
-      int res=0;
-      for(int j=0;j<i;j++){
-        int ret=(y[i]>y[j])+(x[i]>x[j]);
-        if(ret==2){
-          res=1;
-          break;
-        }
-      }
-      if(!res){
-        cnt++;
-      }
+    int n, m, y;
+    int start=1;
+    if (!(cin >> n >> m >> y)) return 0;
+
+    vector<vector<PII>> adj(n+1);
+    for (int i = 0; i < m; ++i) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
     }
-    cout<<cnt;
+    vt<int> x(n+1);
+    for(int i=1;i<=n;i++){
+      cin>>x[i];
+      adj[i].push_back({0,x[i]});
+      adj[0].push_back({i,x[i]+y});
+    }
+    vector<int> dist(n+1, INF);
+  
+    vector<bool> st(n+1, false);
+
+    priority_queue <PII,vector<PII>,greater<PII>> heap;
+
+    dist[start] = 0;
+    heap.push({0, start});
+
+    while (!heap.empty()) {
+        auto [d, u] = heap.top();
+        heap.pop();
+
+        if (st[u]) continue;
+        st[u] = true;
+        for (auto& edge : adj[u]) {
+            int v = edge.first;
+            int w = edge.second;
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                heap.push({dist[v], v}); 
+            }
+        }
+    }
+
+    for (int i = 2; i <= n; ++i) {
+        if (dist[i] == INF) {
+            cout <<x[1]+y+x[i]<< " ";
+        } else {
+            cout <<min(x[1]+x[i]+y,dist[i])<< " ";
+        }
+    }
+
+
+    return 0;
 }
