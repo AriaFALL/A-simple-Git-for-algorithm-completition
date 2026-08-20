9a2fff953f7f48291a86b300f2684eb4d8228830
AriaFAll <ariafall186@gmail.com>
2026-08-20 13:14:32 +0800

    增加注释

--- 其他/头文件前缀.cpp
+++ 其他/头文件前缀.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
 #define int long long
-#define endl "\n"
+//#define endl "\n" /*交互题不可用\n，需要删除此行*/
 #define vt vector
 #define pb push_back
+#define pii pair<int,int>
 using namespace std;
 
 const int INF = 4e18;/*适应大多数情况，防止卡常*/
