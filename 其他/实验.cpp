0752d7c5b14d77092a54dcbf71dcaa86c5ea7371
AriaFAll <ariafall186@gmail.com>
2026-08-20 13:35:08 +0800

    更新实验文件(请勿下载)

---
+++ 其他/实验.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl "\n" /*交互题不可用\n，需要删除此行*/
+#define vt vector
+#define pb push_back
+#define pii pair<int,int>
+using namespace std;
+
+const int INF = 4e18;/*适应大多数情况，防止卡常*/
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t=1;
+    //cin>>t;
+    while(t--){
+        cout<<"hello world";
+    }
+
+
+    return 0;
+}
